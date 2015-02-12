009b305a49e50172193f207f4fdd501fc1947bf7
rubys@intertwingly.net <nielsswimberghe@gmail.com>
2015-02-12 17:09:46 -0500

    Masonry fixed, overview, pagination, start keywords, start categories

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -20,5 +20,71 @@ angular.module('PhotoBrowser', ['ngMaterial','wu.masonry','PhotoAPI'])
     .controller('PhotosController',['$scope','Photo','Category','Keyword','PhotoKeyword','PhotoCategory',
         function($scope,Photo,Category,Keyword,PhotoKeyword,PhotoCategory){
             $scope.photos=Photo.query();
+            $scope.itemsPerPage = 10;
+            $scope.currentPage = 0;
+
+            $scope.range = function() {
+                var rangeSize = 5;
+                var ret = [];
+                var start;
+
+                start = $scope.currentPage;
+                if ( start > $scope.pageCount()-rangeSize ) {
+                    start = $scope.pageCount()-rangeSize+1;
+                }
+
+                for (var i=start; i<start+rangeSize; i++) {
+                    ret.push(i);
+                }
+                return ret;
+            };
+
+            $scope.prevPage = function() {
+                if ($scope.currentPage > 0) {
+                    $scope.currentPage--;
+                }
+            };
+
+            $scope.prevPageDisabled = function() {
+                return $scope.currentPage === 0 ? "disabled" : "";
+            };
+
+            $scope.pageCount = function() {
+                return Math.ceil($scope.photos.length/$scope.itemsPerPage)-1;
+            };
+
+            $scope.nextPage = function() {
+                if ($scope.currentPage < $scope.pageCount()) {
+                    $scope.currentPage++;
+                }
+            };
+
+            $scope.nextPageDisabled = function() {
+                return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+            };
+
+            $scope.setPage = function(n) {
+                $scope.currentPage = n;
+            };
         }
-    ]);
+    ])
+    .controller('CategoriesController',['$scope','Category',function($scope,Category){
+        $scope.categories = Category.query();
+    }])
+    .controller('KeywordsController',['$scope','Keyword',function($scope,Keyword){
+        $scope.keywords = Keyword.query();
+    }])
+    .filter('offset', function() {
+        return function (input, start) {
+            start = parseInt(start, 10);
+            return input.slice(start);
+        }
+    })
+    .filter('range', function() {
+        return function(input, total) {
+            total = parseInt(total);
+            for (var i=0; i<total; i++)
+                input.push(i);
+            return input;
+        };
+    });
