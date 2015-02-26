a6b4e8404ac1a01356ef3aa11b1a04b257af4e23
Niels Swimberghe <nielsswimberghe@gmail.com>
2015-02-26 11:03:52 -0500

    added login style, added tooltips and links back and forth to uploader and browser

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/,/.*\.png/,/.*\.jpeg/]
+Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   get '/', to:'photo_form#index'
